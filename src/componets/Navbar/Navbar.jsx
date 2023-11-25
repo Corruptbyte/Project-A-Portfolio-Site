1d675efe4c3d8f19760f17c627c985d0665def56
Corruptbyte <130069761+Corruptbyte@users.noreply.github.com>
2023-11-24 20:11:40 -0600

    Update Navbar.jsx

--- src/componets/Navbar/Navbar.jsx
+++ src/componets/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
           onClick={() => setMenuOpen(!menuOpen)}
         />
 
-        <ul className={`${styles.menuItems}${menuOpen && styles.me}`}
+        <ul className={`${styles.menuItems}${menuOpen && styles.menuOpen}`}
          onClick={() => setMenuOpen(false)}>
           <li>
             <a href="#About">About</a>
