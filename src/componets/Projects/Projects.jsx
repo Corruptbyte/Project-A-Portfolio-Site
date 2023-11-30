8c1ae4367a2b9e315de11391f4b5d85592a15be3
Corruptbyte <130069761+Corruptbyte@users.noreply.github.com>
2023-11-30 16:13:05 -0600

    Update Projects.jsx

--- src/componets/Projects/Projects.jsx
+++ src/componets/Projects/Projects.jsx
@@ -3,15 +3,18 @@ import styles from "./Projects.module.css";
 import projects from "../../data/Projects.json";
 import { ProjectCard } from "./ProjectCard";
 
+// Component that renders the Projects section
 export const Projects = () => {
-    return (<section className={styles.container} id="Projects">
+    return (
+        <section className={styles.container} id="Projects">
             <h2 className={styles.title}>Projects</h2>
             <div className={styles.projects}>
+                {/* Renders each project card */}
                 {projects.map((project, id) => {
                     return (
                         <ProjectCard key={id} project={project} />
-        ); 
-    })}
+                    );
+                })}
             </div>
         </section>
     );
